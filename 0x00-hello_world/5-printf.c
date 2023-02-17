3f648d38308ba6665ce3904fe799ac4933d6b87d
myUsername <myemail@gmail.com>
2023-02-16 17:05:32 -0800

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 /**
- * main
+ * main - A program printing a line using the print f function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
