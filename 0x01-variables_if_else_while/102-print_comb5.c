a1849188c904bba4febe3a34bc0e32217b5b3370
myUsername <myemail@gmail.com>
2023-02-19 14:04:15 -0800

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 	int i, j;
 
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 100; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = i; j <= 100; j++)
 		{
 			int n1 = i / 10;
 			int n2 = i % 10;
