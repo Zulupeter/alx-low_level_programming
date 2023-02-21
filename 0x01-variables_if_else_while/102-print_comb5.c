65aacd367024d93dc5525695f22251241b9e04fc
myUsername <myemail@gmail.com>
2023-02-20 17:14:11 -0800

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,44 +6,31 @@
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
-	int c2;
-	int f_d2;
-	int l_d2;
+	int i, j;
 
-	while (c <= 98)
+	for (i = 0; i < 98; i++)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c2 = c + 1;
-
-		while (c2 <= 98)
+		for (j = i + 1; j < 100; j++)
 		{
-			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');
-
-			if (c < c2)
+			if (i != j)
 			{
-				putchar(f_d);
-				putchar(l_d);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar(f_d2);
-				putchar(l_d2);
-
-				if (c != 98 || c2 != 99)
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i == 98 && j == 99)
+					continue;
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			c2++;
 		}
-		c++;
-	}
-	putchar('\n');
 
+	}
+
+	putchar(10);
 	return (0);
 
 }
