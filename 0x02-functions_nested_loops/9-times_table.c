ee10dd4202074fc95273febac3c873949c4f4484
myUsername <myemail@gmail.com>
2023-02-21 18:10:07 -0800

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,30 +7,39 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 9; j++)
 		{
 			k = j * i;
-			if (j == 0)
+			if ((k / 10) == 0)
+				if (j == 0)
 			{
-				_putchar(k + '0');
+				_putchar('0');
 			}
 
-			if (k < 10 && j != 0)
+			if (j != 0)
 			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		_putchar('\n');
+		else
+		{
+		_putchar((k / 10) + '0');
+		_putchar((k / 10) + '0');
+		if (j < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
-}
+
+putchar('\n');
+	}
+
