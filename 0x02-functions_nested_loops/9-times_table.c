c95e0db566dbad4c95487caa9eb9455c0e2dc6a3
myUsername <myemail@gmail.com>
2023-02-21 17:14:38 -0800

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j < 9; j++)
 		{
@@ -17,7 +17,7 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
-			if (k < 10 && j != '0')
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
