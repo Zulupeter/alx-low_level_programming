d62a9f2e10da6e75a9ad0e0ce84412488b33ee08
myUsername <myemail@gmail.com>
2023-02-22 11:46:31 -0800

    done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+
+{
+	int i, s = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % % == 0)
+		{
+			s += i;
+		}
+	}
+
+	printf("%d\n", s);
+
+	return (0);
+}
