b848dedf6dc93f183403f6cd09fc2f89a2b2fbbc
myUsername <myemail@gmail.com>
2023-02-23 10:56:52 -0800

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,10 @@
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 for anything else
  */
-int _isdigit(int x)
+int _isupper(int x)
 
 {
-	if (x >= 48 && x <= 57)
+	if (x >= 65 && x <= 90)
 	{
 		return (1);
 	}
