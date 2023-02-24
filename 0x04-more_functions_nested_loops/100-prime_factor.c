7d86b633c15850ee2032603144170284ee753e6a
myUsername <myemail@gmail.com>
2023-02-24 06:23:47 -0800

    done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,17 +9,32 @@
 int main(void)
 
 {
-	long x, maxf;
-	long number = 612852475143;
-	double square = sqrt(number);
+	long int n;
+	long int max;
+	long int i;
 
-	for (x = 1; x <= square; x++)
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-		if (number % x == 0)
+		max = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
 		{
-			maxf = / x;
+			max = i;
+			n - n / i;
 		}
 	}
-	printf("%ld\n", maxf);
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
 	return (0);
 }
