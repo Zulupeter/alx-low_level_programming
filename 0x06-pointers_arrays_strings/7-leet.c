ba3384a063905e44eaf41c8871da2f421954b875
myUsername <myemail@gmail.com>
2023-03-02 05:14:21 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,8 +10,8 @@ char *leet(char *n)
 
 {
 	int i, j;
-	char s1[ ] = "aAeEoOtTlL";
-	char s2[ ] = "4433007711";
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for  (i = 0; n[i] != '\0'; i++)
 	{
@@ -19,7 +19,7 @@ char *leet(char *n)
 		{
 			if (n[i] == s1[j])
 			{
-				n[i] = s2[i];
+				n[i] = s2[j];
 			}
 		}
 	}
