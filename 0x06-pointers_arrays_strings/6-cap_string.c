e12adbdd19b06489c1984908d651565a2d6ccb4f
myUsername <myemail@gmail.com>
2023-03-02 03:55:59 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,39 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include "main.h"
 
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: The string to be capitalized
+ *
+ * Return: A pointer to the changed screen
+ */
 
 char *cap_string(char *str)
 
 {
-	for (int i = 0; str[i] != '\0'; i++)
-	{
-		if (i == 0 || isspace(str[i - 1]) || strchr(",;.!?\"(){}", str[i - 1]))
-		{
-			str[i] = toupper(str[i]);
-		}
+	int index = 0;
+
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
+	{	index++;
+
+	if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+
+		str[index] -= 32;
+
+	index++;
 	}
+
 	return (str);
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,97 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+
+char *leet(char *str)
+{
+	char leet_table[256] = {0};
+	int i, j;
+
+	const char *letters = "aAeEoOtTlL";
+	const char *leet_chars = "44330771";
+
+	for (i = 0; lestr)
+	{
+		char leet_table[256] = {0};
+		int i, j;
+
+		const char *letters = "aAeEoOtTlL";
+		const char *leet_chars = "44330771";
+
+		for (i = 0; letters[i] !=str)
+		{
+			char leet_table[256] = {0};
+			int i, j;
+
+			const char *letters = "aAeEoOtTlL";
+			const char *leet_chars = "44330771";
+
+			for (i = 0; letters[i] != '\0' && lestr)
+			{
+				char leet_table[256] = {0};
+				int i, j;
+
+				const char *letters = "aAeEoOtTlL";
+				const char *leet_chars = "44330771";
+
+				for (i = 0; letters[i] != '\0' && leet_chars[i]str)
+				{
+					char leet_table[256] = {0};
+					int i, j;
+
+					const char *letters = "aAeEoOtTlL";
+					const char *leet_chars = "44330771";
+
+					for (i = 0; letters[i] != '\0' && leet_chars[i] != '\0'; istr)
+					{
+						char leet_table[256] = {0};
+						int i, j;
+
+						const char *letters = "aAeEoOtTlL";
+						const char *leet_chars = "44330771";
+
+						for (i = 0; letters[i] != '\0' && leet_chars[i] != '\0'; i++)
+						{str)
+							{
+								char leet_table[256] = {0};
+								int i, j;
+
+								const char *letters = "aAeEoOtTlL";
+								const char *leet_chars = "44330771";
+
+								for (i = 0; letters[i] != '\0' && leet_chars[i] != '\0'; i++)
+								{
+									leet_tablestr)
+									{
+										char leet_table[256] = {0};
+										int i, j;
+
+										const char *letters = "aAeEoOtTlL";
+										const char *leet_chars = "44330771";
+
+										for (i = 0; letters[i] != '\0' && leet_chars[i] != '\0'; i++)
+										{
+											leet_table[(int)letters[i]] str)
+											{
+												char leet_table[256] = {0};
+												int i, j;
+
+												const char *letters = "aAeEoOtTlL";
+												const char *leet_chars = "44330771";
+
+												for (i = 0; letters[i] != '\0' && leet_chars[i] != '\0'; i++)
+												{
+													leet_table[(int)letters[i]] = leet_chars[i];
+												}
+												for (i = 0; str[i] != '\0'; i++)
+													if (leet_table[(int)str[i]])
+													{
+														str[i] = leet_table[(int)str[i]];
+													}
+										
+
+												return (str);
+											}
