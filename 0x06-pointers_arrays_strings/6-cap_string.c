4b820b9833ccb2b7f8c2446ca4520e6a192b2496
myUsername <myemail@gmail.com>
2023-03-02 04:04:36 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,11 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
+	while (str[index])
+	{
+
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
-	{	index++;
+		index++;
 
 	if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
