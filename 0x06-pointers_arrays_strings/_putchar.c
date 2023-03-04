7643475ff176526626802c2b9f8c053bb736d258
myUsername <myemail@gmail.com>
2023-03-04 09:51:56 -0800

    #include <unistd.h>

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,6 +1,14 @@
-#ifndef _PUTCHAR_H
-#define _PUTCHAR_H
+#include <unistd.h>
+#include <stdio.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
-
-#endif /* _PUTCHAR_H */
+{
+	return (write(1, &c, 1));
+}
