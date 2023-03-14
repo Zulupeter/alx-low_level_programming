749859282dc7a29d9575c37a03ae514f9526e999
myUsername <myemail@gmail.com>
2023-03-14 15:49:20 -0700

    done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,15 +11,13 @@
 
 void free_grid(int **grid, int height)
 {
-	int i;
 
-
-	if (grid != NULL)
+	if (grid != NULL && height != 0)
 
 	{
-		for (i = 0; i < height; i++)
+		for (; height > = 0; height--)
 	{
-		free(grid[i]);
+		free(grid[height]);
 	}
 	free(grid);
 	}
