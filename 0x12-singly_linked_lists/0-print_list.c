541b2ad365771bcdb916621ce0682410bf3620f2
myUsername <myemail@gmail.com>
2023-04-01 13:59:01 -0700

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,21 @@
 
 size_t print_list(const list_t *h)
 {
-size_t nelem;
+	const list_t *temp = h;
+	int node = 0;
 
-nelem = 0;
-for (; h != NULL; h = h->next, nelem++)
-{
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-nelem++;
-}
-return (nelem);
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", temp->len, temp->str);
+		}
+		temp = temp->next;
+		node++;
+	}
+	return (node);
 }
