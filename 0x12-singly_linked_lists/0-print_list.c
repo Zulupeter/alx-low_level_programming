f4bea16c893c9356521174e06ee92e300a8a234d
myUsername <myemail@gmail.com>
2023-03-31 19:10:41 -0700

    done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: head of the singly linked list.
+ * Return: the number of nodes in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nelem;
+
+	nelem = 0;
+	for (; h != NULL; h = h->next, nelem++)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
+}
