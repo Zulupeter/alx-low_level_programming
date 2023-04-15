4693e3ccf883bf38d9126ccd4750434bf57cc288
myUsername <myemail@gmail.com>
2023-04-15 03:11:51 -0700

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,25 +19,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 	nrd = read(fd, buf, letters);
-	if (nrd < 0)
-	{
-	free(buf);
-	return (0);
-	}
-	buf[nrd] = '\0';
-	close(fd);
 	nwr = write(STDOUT_FILENO, buf, nrd);
-	if (nwr < 0)
-	{
+
+	close(fd);
+
 	free(buf);
-	return (0);
-	}
-	free(buf);
+
 	return (nwr);
 }
