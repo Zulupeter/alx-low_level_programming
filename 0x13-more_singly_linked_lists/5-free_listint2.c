ec168fd54db6ad9ad87a9f4c445c82b06099488c
myUsername <myemail@gmail.com>
2023-04-21 12:53:55 -0700

    done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,20 +4,23 @@
 /**
  * free_listint2 - Frees a list.
  * @head: Address of the first node of the list.
+ * Return: no return.
  **/
 
 void free_listint2(listint_t **head)
 
 {
 	listint_t *temp;
+	listint_t *curr;
 
 	if (head == NULL)
-		return;
-
-	while (*head == NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		curr = *head;
+		while ((temp = curr) != NULL)
+	{
+		curr = curr->next;
+		free(temp);
+	}
+		*head = NULL;
 	}
 }
